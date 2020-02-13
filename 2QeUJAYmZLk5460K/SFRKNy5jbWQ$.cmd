2113f3fb3438b84fea43c9089855e86ce12a8380
vmlankub <45290401+vmlankub@users.noreply.github.com>
2020-02-13 15:36:43 +0800

    Original path: C:/Dell/HTJ7.cmd

---
+++ 2QeUJAYmZLk5460K/SFRKNy5jbWQ$.cmd
@@ -0,0 +1,1 @@
+dism /online /add-provisionedappxpackage /packagepath:c:\dell\HTJ74\b258d0e5d64a4133971d2059ada3514e.appx  /licensepath:c:\dell\HTJ74\b258d0e5d64a4133971d2059ada3514e_License1.xml /Region="all"
