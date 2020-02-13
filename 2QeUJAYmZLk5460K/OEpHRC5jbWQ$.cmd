981fe73a7e23b222b17f17e60df638da535a67bc
vmlankub <45290401+vmlankub@users.noreply.github.com>
2020-02-13 15:36:36 +0800

    Original path: C:/Dell/8JGD.cmd

---
+++ 2QeUJAYmZLk5460K/OEpHRC5jbWQ$.cmd
@@ -0,0 +1,1 @@
+dism /online /add-provisionedappxpackage /packagepath:C:\\DELL\8JGDK\a58ba1af2d974b7dac854106da0b7523.appx  /licensepath:C:\\DELL\8JGDK\a58ba1af2d974b7dac854106da0b7523_License1.xml /Region="all"
