27b62c3fe0b191199255da70db6b37782e1d6899
vmlankub <45290401+vmlankub@users.noreply.github.com>
2020-02-13 15:36:08 +0800

    Original path: C:/Dell/2PGX.cmd

---
+++ 2QeUJAYmZLk5460K/MlBHWC5jbWQ$.cmd
@@ -0,0 +1,1 @@
+dism /online /add-provisionedappxpackage /packagepath:C:\\DELL\2PGXF\6b59ecea87ae43ee9b9e3fb725655872.appxbundle  /licensepath:C:\\DELL\2PGXF\6b59ecea87ae43ee9b9e3fb725655872_License1.xml
